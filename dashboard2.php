347b2d2a85bb9dd3b1177ae3dbdd4adacaf84265
Abdirino <abdirinoahash@gmail.com>
2022-11-19 09:30:44 +0300

    dashboard revamp

---
+++ dashboard2.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="dashboard.css">
+    <style>
+
+    </style>
+</head>
+<body>
+    <div class="sidebar">
+        <h1>
+            AL-IBRA  
+        <span><i class='bx bxs-school' style='color:#03fade'  ></i></span></h1>
+    </div>
+
+    <div class="side-menu">
+        <ul>
+            <li>
+                <a href="#">
+                    <span><i class='bx bxs-dashboard' undefined ></i></span>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span><i class='bx bxs-user-pin'></i></span>
+                    <span>Staff</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span><i class='bx bxs-user'></i></span>
+                    <span>Users</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span><i class='bx bxs-message-dots'></i></span>
+                    <span>Enquiries</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span><i class='bx bxs-registered'></i></span>
+                    <span>Registration</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span><i class='bx bxl-invision'></i></span>
+                    <span>Invoice</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span><i class='bx bxs-receipt'></i></span>
+                    <span>Receipt</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span><i class='bx bx-log-out'></i></span>
+                    <span>Logout</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="main-content">
+        <header>
+            <h1>
+                Dashboard
+            </h1>
+
+            <div class="user">
+                <img src="" width="30px" height="30px" alt="">
+            </div>
+            <div>
+                <h4>Abdirino</h4>
+                <small>Super Admin</small>
+            </div>
+        </header>
+
+        <main>
+            <div class="cards">
+                <div class="card">
+                    <span><i class='bx bxs-user'></i></span>
+                    <h2>Users</h2>
+                </div>
+            </div>
+            <div class="cards">
+                <div class="card">
+                    <span><i class='bx bxs-registered'></i></span>
+                    <h2>Registration</h2>
+                </div>
+            </div>
+            <div class="cards">
+                <div class="card">
+                    <span><i class='bx bxs-user-pin'></i></span>
+                    <h2>Staff</h2>
+                </div>
+            </div>
+            <div class="cards">
+                <div class="card">
+                    <span><i class='bx bxs-message-dots'></i></span>
+                    <h2>Enquiries</h2>
+                </div>
+            </div>
+        </main>
+    </div>
+</body>
+</html>
