be6b982328aae7d083f322f3fd60fdcc977be992
Abdirino <abdirinoahash@gmail.com>
2022-11-21 17:19:32 +0300

    navbar start

---
+++ navbar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>school Navbar</title>
+    <link rel="stylesheet" href="navbar.css">
+</head>
+<body>
+    <div class="slider">
+        <div class="logo">
+            <img src="—Pngtree—beautiful cool cartoon cute students_3598172.png" alt="school Logo">
+            <h1>AL-IBRA SCHOOL</h1>
+        </div>
+    </div>
+</body>
+</html>
